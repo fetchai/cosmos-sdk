29e05d9aa1d743a64de34b4b607cd052aab421ce
Alessio Treglia <alessio@tendermint.com>
2020-03-01 17:16:22 +0000

    crypto: cleanup, more testing
    crypto.LedgerShowAddress() is a ledger specific function
    and should takes acc address prefix instead of depending
    on a sdk.Config singleton.
    
    crypto/keys/mintkey:
    - decryptPrivKey() return error instead of os.Exit()ing.
    - encryptPrivKey() should panic instead of os.Exit() when
      bcrypt.GenerateFromPassword() call fails so that the
      caller is given a chance to recover().
    
    crypto/keys:
    - SignWithLedger() does not need to be method of
      baseKeybase. Method receiver is unused.
    - ledgerInfo objecta may be pointers, type switch
      should handle both to avoid panics.
    - keyringKeybase should not provide Update() at all.
    
    More test cases are added to increase package crypto
    and subpackages overall coverage.

--- crypto/keys/mintkey/mintkey_test.go
+++ crypto/keys/mintkey/mintkey_test.go
@@ -3,13 +3,17 @@ package mintkey_test
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/crypto/bcrypt"
+	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/armor"
 	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
 	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/tendermint/tendermint/crypto/xsalsa20symmetric"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/mintkey"
@@ -17,13 +21,48 @@ import (
 
 func TestArmorUnarmorPrivKey(t *testing.T) {
 	priv := secp256k1.GenPrivKey()
-	armor := mintkey.EncryptArmorPrivKey(priv, "passphrase", "")
-	_, _, err := mintkey.UnarmorDecryptPrivKey(armor, "wrongpassphrase")
+	armored := mintkey.EncryptArmorPrivKey(priv, "passphrase", "")
+	_, _, err := mintkey.UnarmorDecryptPrivKey(armored, "wrongpassphrase")
 	require.Error(t, err)
-	decrypted, algo, err := mintkey.UnarmorDecryptPrivKey(armor, "passphrase")
+	decrypted, algo, err := mintkey.UnarmorDecryptPrivKey(armored, "passphrase")
 	require.NoError(t, err)
 	require.Equal(t, string(keys.Secp256k1), algo)
 	require.True(t, priv.Equals(decrypted))
+
+	// empty string
+	decrypted, algo, err = mintkey.UnarmorDecryptPrivKey("", "passphrase")
+	require.Error(t, err)
+	require.True(t, errors.Is(io.EOF, err))
+	require.Nil(t, decrypted)
+	require.Empty(t, algo)
+
+	// wrong key type
+	armored = mintkey.ArmorPubKeyBytes(priv.PubKey().Bytes(), "")
+	decrypted, algo, err = mintkey.UnarmorDecryptPrivKey(armored, "passphrase")
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unrecognized armor type")
+
+	// armor key manually
+	encryptPrivKeyFn := func(privKey crypto.PrivKey, passphrase string) (saltBytes []byte, encBytes []byte) {
+		saltBytes = crypto.CRandBytes(16)
+		key, err := bcrypt.GenerateFromPassword(saltBytes, []byte(passphrase), mintkey.BcryptSecurityParameter)
+		require.NoError(t, err)
+		key = crypto.Sha256(key) // get 32 bytes
+		privKeyBytes := privKey.Bytes()
+		return saltBytes, xsalsa20symmetric.EncryptSymmetric(privKeyBytes, key)
+	}
+	saltBytes, encBytes := encryptPrivKeyFn(priv, "passphrase")
+
+	// wrong kdf header
+	headerWrongKdf := map[string]string{
+		"kdf":  "wrong",
+		"salt": fmt.Sprintf("%X", saltBytes),
+		"type": "secp256k",
+	}
+	armored = armor.EncodeArmor("TENDERMINT PRIVATE KEY", headerWrongKdf, encBytes)
+	_, _, err = mintkey.UnarmorDecryptPrivKey(armored, "passphrase")
+	require.Error(t, err)
+	require.Equal(t, "unrecognized KDF type: wrong", err.Error())
 }
 
 func TestArmorUnarmorPubKey(t *testing.T) {
@@ -49,11 +88,6 @@ func TestArmorUnarmorPubKey(t *testing.T) {
 	require.Equal(t, "unknown", algo)
 	require.True(t, pub.Equals(info.GetPubKey()))
 
-	armored, err = cstore.ExportPrivKey("Bob", "passphrase", "alessio")
-	require.NoError(t, err)
-	_, _, err = mintkey.UnarmorPubKeyBytes(armored)
-	require.Equal(t, `couldn't unarmor bytes: unrecognized armor type "TENDERMINT PRIVATE KEY", expected: "TENDERMINT PUBLIC KEY"`, err.Error())
-
 	// armor pubkey manually
 	header := map[string]string{
 		"version": "0.0.0",
@@ -74,19 +108,7 @@ func TestArmorUnarmorPubKey(t *testing.T) {
 	require.Nil(t, bz)
 	require.Empty(t, algo)
 	require.Error(t, err)
-	require.Equal(t, "header's version field is empty", err.Error())
-
-	// unknown version header
-	header = map[string]string{
-		"type":    "unknown",
-		"version": "unknown",
-	}
-	armored = armor.EncodeArmor("TENDERMINT PUBLIC KEY", header, pubBytes)
-	bz, algo, err = mintkey.UnarmorPubKeyBytes(armored)
-	require.Nil(t, bz)
-	require.Empty(t, algo)
-	require.Error(t, err)
-	require.Equal(t, "unrecognized version: unknown", err.Error())
+	require.Contains(t, err.Error(), "unrecognized version")
 }
 
 func TestArmorInfoBytes(t *testing.T) {
