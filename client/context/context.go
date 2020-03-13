b0c85238f9a51bd0f32c32091c58f43c09787e16
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-03-13 13:59:14 -0400

    Update context

--- client/context/context.go
+++ client/context/context.go
@@ -15,6 +15,7 @@ import (
 	rpcclienthttp "github.com/tendermint/tendermint/rpc/client/http"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	clientx "github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -26,6 +27,8 @@ type CLIContext struct {
 	FromAddress   sdk.AccAddress
 	Client        rpcclient.Client
 	ChainID       string
+	TxGenerator   clientx.TxGenerator
+	Marshaler     codec.Marshaler
 	Keybase       keys.Keybase
 	Input         io.Reader
 	Output        io.Writer
@@ -37,13 +40,15 @@ type CLIContext struct {
 	BroadcastMode string
 	Verifier      tmlite.Verifier
 	FromName      string
-	Codec         *codec.Codec
 	TrustNode     bool
 	UseLedger     bool
 	Simulate      bool
 	GenerateOnly  bool
 	Indent        bool
 	SkipConfirm   bool
+
+	// TODO: Deprecated (remove).
+	Codec *codec.Codec
 }
 
 // NewCLIContextWithInputAndFrom returns a new initialized CLIContext with parameters from the
@@ -131,7 +136,20 @@ func (ctx CLIContext) WithInput(r io.Reader) CLIContext {
 	return ctx
 }
 
+// WithTxGenerator returns a copy of the CLIContext with an updated TxGenerator.
+func (ctx CLIContext) WithTxGenerator(txg clientx.TxGenerator) CLIContext {
+	ctx.TxGenerator = txg
+	return ctx
+}
+
+// WithMarshaler returns a copy of the CLIContext with an updated Marshaler.
+func (ctx CLIContext) WithMarshaler(m codec.Marshaler) CLIContext {
+	ctx.Marshaler = m
+	return ctx
+}
+
 // WithCodec returns a copy of the context with an updated codec.
+// TODO: Deprecated (remove).
 func (ctx CLIContext) WithCodec(cdc *codec.Codec) CLIContext {
 	ctx.Codec = cdc
 	return ctx
