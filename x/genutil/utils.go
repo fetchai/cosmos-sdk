7bf8c68d01ff6ddeb340c3727ab2fa572bb018be
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2020-04-06 12:33:15 -0400

    Merge PR #5938: x/genutil: fix InitializeNodeValidatorFiles

--- x/genutil/utils.go
+++ x/genutil/utils.go
@@ -50,19 +50,19 @@ func InitializeNodeValidatorFiles(config *cfg.Config,
 
 	nodeKey, err := p2p.LoadOrGenNodeKey(config.NodeKeyFile())
 	if err != nil {
-		return nodeID, valPubKey, err
+		return "", nil, err
 	}
 
 	nodeID = string(nodeKey.ID())
 
 	pvKeyFile := config.PrivValidatorKeyFile()
 	if err := tmos.EnsureDir(filepath.Dir(pvKeyFile), 0777); err != nil {
-		return nodeID, valPubKey, nil
+		return "", nil, err
 	}
 
 	pvStateFile := config.PrivValidatorStateFile()
 	if err := tmos.EnsureDir(filepath.Dir(pvStateFile), 0777); err != nil {
-		return nodeID, valPubKey, nil
+		return "", nil, err
 	}
 
 	valPubKey, err = privval.LoadOrGenFilePV(pvKeyFile, pvStateFile).GetPubKey()
