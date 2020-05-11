2d2b6f6c975e9e949799f10c3c2cf576035d6903
Ed FitzGerald <edward.fitzgerald@fetch.ai>
2020-05-11 18:44:40 +0100

    Switch to "fet" denomination

--- types/staking.go
+++ types/staking.go
@@ -8,7 +8,7 @@ import (
 const (
 
 	// default bond denomination
-	DefaultBondDenom = "ufet"
+	DefaultBondDenom = "fet"
 
 	// Delay, in blocks, between when validator updates are returned to the
 	// consensus-engine and when they are applied. For example, if
