6e6c85fb7878ee3c9bac040a98457a356e3f90f7
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2020-07-06 15:50:09 -0400

    keys: CLI Remove Viper (#6613)
    * init commit
    
    * Update ListKeysCmd
    
    * updates
    
    * updates
    
    * test updates
    
    * fix tests
    
    * keys: Fix Test_runAddCmdLedgerWithCustomCoinType
    
    * keys: Fix Test_runAddCmdLedger
    
    * keys: Fix Test_runAddCmdBasic
    
    * keys: Fix root tests
    
    * keys: Fix Test_runMigrateCmd
    
    * keys: Fix Test_runListCmd
    
    * keys: Fix remaining tests

--- client/keys/import.go
+++ client/keys/import.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/input"
@@ -28,53 +27,30 @@ func ImportKeyCommand() *cobra.Command {
 		Short: "Import private keys into the local keybase",
 		Long:  "Import a ASCII armored private key into the local keybase.",
 		Args:  cobra.ExactArgs(2),
-		RunE:  runImportCmd,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			buf := bufio.NewReader(cmd.InOrStdin())
+
+			backend, _ := cmd.Flags().GetString(flags.FlagKeyringBackend)
+			homeDir, _ := cmd.Flags().GetString(flags.FlagHome)
+			kb, err := keyring.New(sdk.KeyringServiceName(), backend, homeDir, buf)
+			if err != nil {
+				return err
+			}
+
+			bz, err := ioutil.ReadFile(args[1])
+			if err != nil {
+				return err
+			}
+
+			passphrase, err := input.GetPassword("Enter passphrase to decrypt your key:", buf)
+			if err != nil {
+				return err
+			}
+
+			return kb.ImportPrivKey(args[0], string(bz), passphrase)
+		},
 	}
 	cmd.Flags().Bool(flagAgentRawKey, false, "Signal that you want to import an key from the agent framework")
 
 	return cmd
-}
-
-func runImportCmd(cmd *cobra.Command, args []string) error {
-	buf := bufio.NewReader(cmd.InOrStdin())
-	kb, err := keyring.New(sdk.KeyringServiceName(), viper.GetString(flags.FlagKeyringBackend), viper.GetString(flags.FlagHome), buf)
-	if err != nil {
-		return err
-	}
-
-	bz, err := ioutil.ReadFile(args[1])
-	if err != nil {
-		return err
-	}
-
-	// if the user has requested that a raw agent key is imported then we must read the file as such
-	if viper.GetBool(flagAgentRawKey) {
-		// hex decode the input string
-		rawPrivKey, err := hex.DecodeString(string(bz))
-		if err != nil {
-			return err
-		}
-
-		// check the size of the binary data
-		if len(rawPrivKey) != 32 {
-			return errors.New("Incorrect raw key size. Please check input path")
-		}
-
-		// create the underlying private key
-		var priv secp256k1.PrivKeySecp256k1
-		copy(priv[:], rawPrivKey)
-
-		// armor and encrypt the key with a dummy password (to reuse existing private key import path)
-		passPhrase := "dummy-not-really-used"
-		armored := mintkey.EncryptArmorPrivKey(priv, passPhrase, "secp256k1")
-
-		return kb.ImportPrivKey(args[0], armored, passPhrase)
-	}
-
-	passphrase, err := input.GetPassword("Enter passphrase to decrypt your key:", buf)
-	if err != nil {
-		return err
-	}
-
-	return kb.ImportPrivKey(args[0], string(bz), passphrase)
 }
