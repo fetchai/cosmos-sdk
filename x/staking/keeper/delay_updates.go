8d5ed2f2ab7b419c8d8883754e126217fa06a4bb
jinmannwong <jinmannwong@gmail.com>
2020-08-06 11:51:49 +0100

    Remove turning off of update delays

--- x/staking/keeper/delay_updates.go
+++ x/staking/keeper/delay_updates.go
@@ -7,13 +7,12 @@ import (
 )
 
 var (
-	delayUpdates = true
 	validatorUpdateKey = []byte("validatorUpdateKey")
 )
 
 func (k Keeper) CheckValidatorUpdates(ctx sdk.Context, header abci.Header) {
 	// If two blocks before an next aeon start, need to return new validator set of next aeon
-	if !delayUpdates || header.Entropy.GetRound() == header.Entropy.GetAeonLength()-2 {
+	if header.Entropy.GetRound() == header.Entropy.GetAeonLength()-2 {
 		store := ctx.KVStore(k.storeKey)
 		store.Set(validatorUpdateKey, []byte{0})
 	}
