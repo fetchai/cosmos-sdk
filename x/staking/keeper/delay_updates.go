fc5d0eb47dba4a5aeb02196392cf367ea30d7ba0
jinmannwong <jinmannwong@gmail.com>
2020-08-06 11:37:46 +0100

    Remove print statements

--- x/staking/keeper/delay_updates.go
+++ x/staking/keeper/delay_updates.go
@@ -1,24 +1,21 @@
 package keeper
 
 import (
-	"fmt"
-
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var (
+	delayUpdates = true
 	validatorUpdateKey = []byte("validatorUpdateKey")
 )
 
 func (k Keeper) CheckValidatorUpdates(ctx sdk.Context, header abci.Header) {
 	// If two blocks before an next aeon start, need to return new validator set of next aeon
-	fmt.Printf("beginBlock: entropy round %v, aeon length %v \n", header.Entropy.GetRound(), header.Entropy.GetAeonLength())
-	if header.Entropy.GetRound() == header.Entropy.GetAeonLength()-2 {
+	if !delayUpdates || header.Entropy.GetRound() == header.Entropy.GetAeonLength()-2 {
 		store := ctx.KVStore(k.storeKey)
 		store.Set(validatorUpdateKey, []byte{0})
-		fmt.Printf("beginBlock: changeover validators set \n")
 	}
 }
 
@@ -28,6 +25,5 @@ func (k Keeper) PerformValidatorUpdates(ctx sdk.Context) bool {
 		return false
 	}
 	store.Set(validatorUpdateKey, []byte{})
-	fmt.Printf("endBlock: compute validator updates \n")
 	return true
 }
