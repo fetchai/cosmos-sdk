d5348f0ca5cce5424909d94af62de37cb24e73e7
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2020-10-08 13:41:35 -0400

    Merge PR #7485: keyring-dir flag

--- client/keys/import.go
+++ client/keys/import.go
@@ -8,16 +8,8 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/input"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/tendermint/tendermint/crypto/secp256k1"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/mintkey"
-)
-
-const (
-	flagAgentRawKey = "agent-raw-key"
 )
 
 // ImportKeyCommand imports private keys from a keyfile.
@@ -29,13 +21,7 @@ func ImportKeyCommand() *cobra.Command {
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			buf := bufio.NewReader(cmd.InOrStdin())
-
-			backend, _ := cmd.Flags().GetString(flags.FlagKeyringBackend)
-			homeDir, _ := cmd.Flags().GetString(flags.FlagHome)
-			kb, err := keyring.New(sdk.KeyringServiceName(), backend, homeDir, buf)
-			if err != nil {
-				return err
-			}
+			clientCtx := client.GetClientContextFromCmd(cmd)
 
 			bz, err := ioutil.ReadFile(args[1])
 			if err != nil {
@@ -47,7 +33,7 @@ func ImportKeyCommand() *cobra.Command {
 				return err
 			}
 
-			return kb.ImportPrivKey(args[0], string(bz), passphrase)
+			return clientCtx.Keyring.ImportPrivKey(args[0], string(bz), passphrase)
 		},
 	}
 	cmd.Flags().Bool(flagAgentRawKey, false, "Signal that you want to import an key from the agent framework")
