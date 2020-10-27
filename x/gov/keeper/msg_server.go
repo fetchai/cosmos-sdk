85c4421a082cccba9cd325496ac4a50949f4c15d
antstalepresh <stalepresh121@outlook.com>
2020-10-27 21:50:11 +1000

    basic migration for SubVote type

--- x/gov/keeper/msg_server.go
+++ x/gov/keeper/msg_server.go
@@ -65,7 +65,7 @@ func (k msgServer) Vote(goCtx context.Context, msg *types.MsgVote) (*types.MsgVo
 	if accErr != nil {
 		return nil, accErr
 	}
-	err := k.Keeper.AddVote(ctx, msg.ProposalId, accAddr, msg.Option)
+	err := k.Keeper.AddVote(ctx, msg.ProposalId, accAddr, msg.SubVotes)
 	if err != nil {
 		return nil, err
 	}
