dde2c800637ec4645f9fb8fc3e787c19fa97551b
Ed FitzGerald <edward.fitzgerald@fetch.ai>
2020-12-08 15:27:38 +0000

    backport: add support for importing raw agent keys
    Original PR: #644

--- client/keys/import.go
+++ client/keys/import.go
@@ -2,6 +2,8 @@ package keys
 
 import (
 	"bufio"
+	"encoding/hex"
+	"errors"
 	"io/ioutil"
 
 	"github.com/spf13/cobra"
@@ -11,17 +13,26 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/input"
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/mintkey"
+)
+
+const (
+	flagAgentRawKey = "agent-raw-key"
 )
 
 // ImportKeyCommand imports private keys from a keyfile.
 func ImportKeyCommand() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "import <name> <keyfile>",
 		Short: "Import private keys into the local keybase",
 		Long:  "Import a ASCII armored private key into the local keybase.",
 		Args:  cobra.ExactArgs(2),
 		RunE:  runImportCmd,
 	}
+	cmd.Flags().Bool(flagAgentRawKey, false, "Signal that you want to import an key from the agent framework")
+
+	return cmd
 }
 
 func runImportCmd(cmd *cobra.Command, args []string) error {
@@ -34,6 +45,30 @@ func runImportCmd(cmd *cobra.Command, args []string) error {
 	bz, err := ioutil.ReadFile(args[1])
 	if err != nil {
 		return err
+	}
+
+	// if the user has requested that a raw agent key is imported then we must read the file as such
+	if viper.GetBool(flagAgentRawKey) {
+		// hex decode the input string
+		rawPrivKey, err := hex.DecodeString(string(bz))
+		if err != nil {
+			return err
+		}
+
+		// check the size of the binary data
+		if len(rawPrivKey) != 32 {
+			return errors.New("Incorrect raw key size. Please check input path")
+		}
+
+		// create the underlying private key
+		var priv secp256k1.PrivKeySecp256k1
+		copy(priv[:], rawPrivKey)
+
+		// armor and encrypt the key with a dummy password (to reuse existing private key import path)
+		passPhrase := "dummy-not-really-used"
+		armored := mintkey.EncryptArmorPrivKey(priv, passPhrase, "secp256k1")
+
+		return kb.ImportPrivKey(args[0], armored, passPhrase)
 	}
 
 	passphrase, err := input.GetPassword("Enter passphrase to decrypt your key:", buf)
