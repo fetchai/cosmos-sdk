7e42b4c525364494fc4d3e5d5eaa038a11b08287
Ed FitzGerald <edward.fitzgerald@fetch.ai>
2021-02-17 10:33:32 +0000

    feat: fixed rate minting rewards (#71)

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -13,8 +13,7 @@ func BeginBlocker(ctx sdk.Context, k Keeper) {
 
 	// recalculate inflation rate
 	totalStakingSupply := k.StakingTokenSupply(ctx)
-	bondedRatio := k.BondedRatio(ctx)
-	minter.Inflation = minter.NextInflationRate(params, bondedRatio)
+	minter.Inflation = minter.NextInflationRate(params)
 	minter.AnnualProvisions = minter.NextAnnualProvisions(params, totalStakingSupply)
 	k.SetMinter(ctx, minter)
 
@@ -36,7 +35,6 @@ func BeginBlocker(ctx sdk.Context, k Keeper) {
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
 			types.EventTypeMint,
-			sdk.NewAttribute(types.AttributeKeyBondedRatio, bondedRatio.String()),
 			sdk.NewAttribute(types.AttributeKeyInflation, minter.Inflation.String()),
 			sdk.NewAttribute(types.AttributeKeyAnnualProvisions, minter.AnnualProvisions.String()),
 			sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
