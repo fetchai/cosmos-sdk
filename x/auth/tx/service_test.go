f47729af0f263de5d6f856a5f91877da6c4c93a7
Amaury <1293565+amaurym@users.noreply.github.com>
2021-02-18 19:00:19 +0100

    Use service Msgs in CLI tx commands (#8512)
    * Use service Msgs in CLI tx commands
    
    * Update comment
    
    * Gracefully support amino signing
    
    * CLI to use svc msg
    
    * Fix lint
    
    * Use fq method name in events
    
    * Update tests
    
    * Fix quering events
    
    * Add docs
    
    * Fix test build
    
    * Fix events
    
    * Fix search for events
    
    * Handle old andd new event quering
    
    * Use merge events
    
    * Better encCfg
    
    * Add page in search
    
    * Fix tests
    
    * Update test and comments
    
    * Update x/auth/legacy/legacytx/stdsign.go
    
    Co-authored-by: atheeshp <59333759+atheeshp@users.noreply.github.com>
    
    * Fix conflict for weighted vote
    
    * Make CopyTx actually run
    
    * Fix CopyTx
    
    * Fix test
    
    * Add changelog entry
    
    * Remove useless code
    
    * Add msgs tests
    
    * Remove testing proto Msg via CLI
    
    * Fix lint
    
    * Fix test
    
    * Implement GetSignBytes on ServiceMsg
    
    * Fix %T
    
    Co-authored-by: atheeshp <59333759+atheeshp@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- x/auth/tx/service_test.go
+++ x/auth/tx/service_test.go
@@ -183,14 +183,14 @@ func (s IntegrationTestSuite) TestGetTxEvents_GRPC() {
 		{
 			"without pagination",
 			&tx.GetTxsEventRequest{
-				Events: []string{"message.action='send'"},
+				Events: []string{"message.action=/cosmos.bank.v1beta1.Msg/Send"},
 			},
 			false, "",
 		},
 		{
 			"with pagination",
 			&tx.GetTxsEventRequest{
-				Events: []string{"message.action='send'"},
+				Events: []string{"message.action=/cosmos.bank.v1beta1.Msg/Send"},
 				Pagination: &query.PageRequest{
 					CountTotal: false,
 					Offset:     0,
@@ -202,7 +202,7 @@ func (s IntegrationTestSuite) TestGetTxEvents_GRPC() {
 		{
 			"with multi events",
 			&tx.GetTxsEventRequest{
-				Events: []string{"message.action='send'", "message.module='bank'"},
+				Events: []string{"message.action=/cosmos.bank.v1beta1.Msg/Send", "message.module=bank"},
 			},
 			false, "",
 		},
@@ -245,25 +245,13 @@ func (s IntegrationTestSuite) TestGetTxEvents_GRPCGateway() {
 		},
 		{
 			"without pagination",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s", val.APIAddress, "message.action='send'"),
+			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s", val.APIAddress, "message.action=/cosmos.bank.v1beta1.Msg/Send"),
 			false,
 			"",
 		},
 		{
 			"with pagination",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&pagination.offset=%d&pagination.limit=%d", val.APIAddress, "message.action='send'", 0, 10),
-			false,
-			"",
-		},
-		{
-			"valid request: order by asc",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&events=%s&order_by=ORDER_BY_ASC", val.APIAddress, "message.action='send'", "message.module='bank'"),
-			false,
-			"",
-		},
-		{
-			"valid request: order by desc",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&events=%s&order_by=ORDER_BY_DESC", val.APIAddress, "message.action='send'", "message.module='bank'"),
+			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&pagination.offset=%d&pagination.limit=%d", val.APIAddress, "message.action=/cosmos.bank.v1beta1.Msg/Send", 0, 10),
 			false,
 			"",
 		},
@@ -275,13 +263,13 @@ func (s IntegrationTestSuite) TestGetTxEvents_GRPCGateway() {
 		},
 		{
 			"expect pass with multiple-events",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&events=%s", val.APIAddress, "message.action='send'", "message.module='bank'"),
+			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s&events=%s", val.APIAddress, "message.action=/cosmos.bank.v1beta1.Msg/Send", "message.module=bank"),
 			false,
 			"",
 		},
 		{
 			"expect pass with escape event",
-			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s", val.APIAddress, "message.action%3D'send'"),
+			fmt.Sprintf("%s/cosmos/tx/v1beta1/txs?events=%s", val.APIAddress, "message.action%3D%2Fcosmos.bank.v1beta1.Msg%2FSend"),
 			false,
 			"",
 		},
