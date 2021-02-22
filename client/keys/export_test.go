dcf7c2604e715fb4c0bd4041463826b3213b9cac
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-02-22 16:14:09 +0100

    Deprecate SetFullFundraiserPath in favor of SetPurpose and SetCoinType (#8629)
    Co-authored-by: Alessio Treglia <alessio@tendermint.com>

--- client/keys/export_test.go
+++ client/keys/export_test.go
@@ -18,51 +18,29 @@ import (
 )
 
 func Test_runExportCmd(t *testing.T) {
-	testCases := []struct {
-		name           string
-		keyringBackend string
-		extraArgs      []string
-		userInput      string
-		mustFail       bool
-		expectedOutput string
-	}{
-		{
-			name:           "--unsafe only must fail",
-			keyringBackend: keyring.BackendTest,
-			extraArgs:      []string{"--unsafe"},
-			mustFail:       true,
-		},
-		{
-			name:           "--unarmored-hex must fail",
-			keyringBackend: keyring.BackendTest,
-			extraArgs:      []string{"--unarmored-hex"},
-			mustFail:       true,
-		},
-		{
-			name:           "--unsafe --unarmored-hex fail with no user confirmation",
-			keyringBackend: keyring.BackendTest,
-			extraArgs:      []string{"--unsafe", "--unarmored-hex"},
-			userInput:      "",
-			mustFail:       true,
-			expectedOutput: "",
-		},
-		{
-			name:           "--unsafe --unarmored-hex succeed",
-			keyringBackend: keyring.BackendTest,
-			extraArgs:      []string{"--unsafe", "--unarmored-hex"},
-			userInput:      "y\n",
-			mustFail:       false,
-			expectedOutput: "2485e33678db4175dc0ecef2d6e1fc493d4a0d7f7ce83324b6ed70afe77f3485\n",
-		},
-		{
-			name:           "file keyring backend properly read password and user confirmation",
-			keyringBackend: keyring.BackendFile,
-			extraArgs:      []string{"--unsafe", "--unarmored-hex"},
-			// first 2 pass for creating the key, then unsafe export confirmation, then unlock keyring pass
-			userInput:      "12345678\n12345678\ny\n12345678\n",
-			mustFail:       false,
-			expectedOutput: "2485e33678db4175dc0ecef2d6e1fc493d4a0d7f7ce83324b6ed70afe77f3485\n",
-		},
+	cmd := ExportKeyCommand()
+	cmd.Flags().AddFlagSet(Commands("home").PersistentFlags())
+	mockIn := testutil.ApplyMockIODiscardOutErr(cmd)
+
+	// Now add a temporary keybase
+	kbHome := t.TempDir()
+
+	// create a key
+	kb, err := keyring.New(sdk.KeyringServiceName(), keyring.BackendTest, kbHome, mockIn)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		kb.Delete("keyname1") // nolint:errcheck
+	})
+
+	path := sdk.GetConfig().GetFullBIP44Path()
+	_, err = kb.NewAccount("keyname1", testutil.TestMnemonic, "", path, hd.Secp256k1)
+	require.NoError(t, err)
+
+	// Now enter password
+	args := []string{
+		"keyname1",
+		fmt.Sprintf("--%s=%s", flags.FlagHome, kbHome),
+		fmt.Sprintf("--%s=%s", flags.FlagKeyringBackend, keyring.BackendTest),
 	}
 
 	for _, tc := range testCases {
