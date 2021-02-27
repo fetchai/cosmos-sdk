23e8fdca6c1a9b489b01b096e50b5ff881a1e8a2
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-02-27 07:26:22 -0800

    store/cachekv, x/bank/types: algorithmically fix pathologically slow code (#8719)
    After continuously profiling InitGensis with 100K accounts, it showed
    pathologically slow code, that was the result of a couple of patterns:
    * Unconditional and not always necessary map lookups
    * O(n^2) sdk.AccAddressFromBech32 retrievals when the code is expensive,
    during a quicksort
    
    The remedy involved 4 parts:
    * O(n) sdk.AccAddressFromBech32 invocations, down from O(n^2) in the quicksort
    * Only doing map lookups when the domain key check has passed
    * Using a black magic compiler technique of the map clearing idiom
    * Zero allocation []byte<->string conversion
    
    With 100K accounts, this brings InitGenesis down to ~6min, instead of
    20+min, it reduces the sort code from ~7sec down to 50ms.
    
    Also some simple benchmark reflect the change:
    ```shell
    name                    old time/op    new time/op    delta
    SanitizeBalances500-8     19.3ms ±10%     1.5ms ± 5%  -92.46%  (p=0.000 n=20+20)
    SanitizeBalances1000-8    41.9ms ± 8%     3.0ms ±12%  -92.92%  (p=0.000 n=20+20)
    
    name                    old alloc/op   new alloc/op   delta
    SanitizeBalances500-8     9.05MB ± 6%    0.56MB ± 0%  -93.76%  (p=0.000 n=20+18)
    SanitizeBalances1000-8    20.2MB ± 3%     1.1MB ± 0%  -94.37%  (p=0.000 n=20+19)
    
    name                    old allocs/op  new allocs/op  delta
    SanitizeBalances500-8      72.4k ± 6%      4.5k ± 0%  -93.76%  (p=0.000 n=20+20)
    SanitizeBalances1000-8      162k ± 3%        9k ± 0%  -94.40%  (p=0.000 n=20+20)
    ```
    
    The CPU profiles show the radical change as per
    https://github.com/cosmos/cosmos-sdk/issues/7766#issuecomment-786671734
    
    Later on, we shall do more profiling and fixes but for now this brings
    down the run-time for InitGenesis.
    
    Fixes #7766
    
    Co-authored-by: Alessio Treglia <alessio@tendermint.com>

--- store/cachekv/store.go
+++ store/cachekv/store.go
@@ -182,7 +182,7 @@ func (store *Store) iterator(start, end []byte, ascending bool) types.Iterator {
 // from string -> []byte to speed up operations, it is not meant
 // to be used generally, but for a specific pattern to check for available
 // keys within a domain.
-func strToBytes(s string) []byte {
+func strToByte(s string) []byte {
 	var b []byte
 	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	hdr.Cap = len(s)
@@ -204,31 +204,15 @@ func byteSliceToStr(b []byte) string {
 func (store *Store) dirtyItems(start, end []byte) {
 	n := len(store.unsortedCache)
 	unsorted := make([]*kv.Pair, 0)
-	// If the unsortedCache is too big, its costs too much to determine
-	// whats in the subset we are concerned about.
-	// If you are interleaving iterator calls with writes, this can easily become an
-	// O(N^2) overhead.
-	// Even without that, too many range checks eventually becomes more expensive
-	// than just not having the cache.
-	if n >= 1024 {
-		for key := range store.unsortedCache {
+
+	n := len(store.unsortedCache)
+	for key := range store.unsortedCache {
+		if dbm.IsKeyInDomain(strToByte(key), start, end) {
 			cacheValue := store.cache[key]
 			unsorted = append(unsorted, &kv.Pair{Key: []byte(key), Value: cacheValue.value})
 		}
-	} else {
-		// else do a linear scan to determine if the unsorted pairs are in the pool.
-		for key := range store.unsortedCache {
-			if dbm.IsKeyInDomain(strToBytes(key), start, end) {
-				cacheValue := store.cache[key]
-				unsorted = append(unsorted, &kv.Pair{Key: []byte(key), Value: cacheValue.value})
-			}
-		}
 	}
-	store.clearUnsortedCacheSubset(unsorted)
-}
 
-func (store *Store) clearUnsortedCacheSubset(unsorted []*kv.Pair) {
-	n := len(store.unsortedCache)
 	if len(unsorted) == n { // This pattern allows the Go compiler to emit the map clearing idiom for the entire map.
 		for key := range store.unsortedCache {
 			delete(store.unsortedCache, key)
