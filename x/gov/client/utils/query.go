327be47d30f45ff1b9f270dc03908d27c5b1d0b7
Amaury <1293565+amaurym@users.noreply.github.com>
2021-05-03 16:45:47 +0200

    Rename clientCtx.JSONMarshaler to JSONCodec (#9251)
    * Rename clientCtx.JSONMarshaler to JSONCodec
    
    * change md files
    
    * Fix lint

--- x/gov/client/utils/query.go
+++ x/gov/client/utils/query.go
@@ -208,7 +208,7 @@ func QueryVoteByTxQuery(clientCtx client.Context, params types.QueryVoteParams) 
 			}
 
 			if vote != nil {
-				bz, err := clientCtx.JSONMarshaler.MarshalJSON(vote)
+				bz, err := clientCtx.JSONCodec.MarshalJSON(vote)
 				if err != nil {
 					return nil, err
 				}
@@ -253,7 +253,7 @@ func QueryDepositByTxQuery(clientCtx client.Context, params types.QueryDepositPa
 					Amount:     depMsg.Amount,
 				}
 
-				bz, err := clientCtx.JSONMarshaler.MarshalJSON(&deposit)
+				bz, err := clientCtx.JSONCodec.MarshalJSON(&deposit)
 				if err != nil {
 					return nil, err
 				}
