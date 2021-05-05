510f518a5d1df9f3770a1860e547c0e60ec52f29
Flavien Binet <flavien.binet@gmail.com>
2021-05-05 19:02:15 +0200

    chores: cleanup unused bondedRatio

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -19,7 +19,6 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 
 	// recalculate inflation rate
 	totalStakingSupply := k.StakingTokenSupply(ctx)
-	bondedRatio := k.BondedRatio(ctx)
 	minter.Inflation = minter.NextInflationRate(params)
 	minter.AnnualProvisions = minter.NextAnnualProvisions(params, totalStakingSupply)
 	k.SetMinter(ctx, minter)
@@ -46,7 +45,6 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
 			types.EventTypeMint,
-			sdk.NewAttribute(types.AttributeKeyBondedRatio, bondedRatio.String()),
 			sdk.NewAttribute(types.AttributeKeyInflation, minter.Inflation.String()),
 			sdk.NewAttribute(types.AttributeKeyAnnualProvisions, minter.AnnualProvisions.String()),
 			sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
