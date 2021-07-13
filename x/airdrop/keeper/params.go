532f7bd07c27fef5150c3a707d82563bed9b33f5
Joey <jonathan.sumner@fetch.ai>
2021-07-13 17:19:58 +0100

    Fix: let AllowList be empty

--- x/airdrop/keeper/params.go
+++ x/airdrop/keeper/params.go
@@ -7,7 +7,7 @@ import (
 
 func (k Keeper) GetWhiteListClients(ctx sdk.Context) []string {
 	var res []string
-	k.paramSpace.GetIfExists(ctx, types.KeyWhiteList, &res)	// Allow empty AllowList
+	k.paramSpace.GetIfExists(ctx, types.KeyWhiteList, &res)
 	return res
 }
 
