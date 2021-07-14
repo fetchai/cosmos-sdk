a84849ff7d7c689fd78e0445a6eb41e50ea4ad23
Joey <jonathan.sumner@fetch.ai>
2021-07-14 11:50:56 +0100

    Refactor: increase test coverage

--- x/airdrop/types/genesis_test.go
+++ x/airdrop/types/genesis_test.go
@@ -2,65 +2,69 @@ package types_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/cosmos/cosmos-sdk/x/airdrop/types"
+	"github.com/stretchr/testify/require"
 
-	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 var (
-	addr = sdk.AccAddress([]byte("addr________________"))
-	//verboseAddr = sdk.AccAddress([]byte("\n\n\n\n\taddr________________\t\n\n\n\n\n"))
+	addr        = sdk.AccAddress([]byte("addr________________"))
+	verboseAddr = sdk.AccAddress([]byte("\n\n\n\n\taddr________________\t\n\n\n\n\n"))
 )
 
 type GenesisTestSuite struct {
 	suite.Suite
-
-	app *simapp.SimApp
-	ctx sdk.Context
 }
 
-func (s *GenesisTestSuite) SetupTest() {
-	app := simapp.Setup(false)
-	s.app = app
-	s.ctx = app.BaseApp.NewContext(false, tmproto.Header{
-		Time:   time.Now(),
-		Height: 10,
-	})
-	s.app.AirdropKeeper.SetParams(s.ctx, types.NewParams(addr.String()))
+func TestNewGenesisState(t *testing.T) {
+	p := types.NewParams()
+	expectedFunds := []types.ActiveFund{
+		{
+			Sender: addr.String(),
+			Fund: &types.Fund{
+				Amount:     &sdk.Coin{Denom: "test", Amount: sdk.NewInt(10)},
+				DripAmount: sdk.NewInt(1),
+			},
+		},
+	}
+	expectedState := &types.GenesisState{
+		Params: p,
+		Funds:  expectedFunds,
+	}
+	require.Equal(t, expectedState.GetFunds(), expectedFunds)
+	require.Equal(t, expectedState.Params, p)
 }
 
-func (s *GenesisTestSuite) TestNewGenesisState() {
-	p := s.app.AirdropKeeper.GetParams(s.ctx)
-	funds, err := s.app.AirdropKeeper.GetActiveFunds(s.ctx)
-	if err != nil {
-		s.Require().FailNow("Failed to get active funds")
+func TestValidateGenesisState(t *testing.T) {
+	p1 := types.Params{
+		AllowList: []string{
+			addr.String(), // valid address
+		},
 	}
-	s.Require().IsType(types.GenesisState{}, *types.NewGenesisState(p, funds))
-}
-
-func (s *GenesisTestSuite) TestValidateGenesisState() {
-	p := s.app.AirdropKeeper.GetParams(s.ctx)
-	activeFunds, err := s.app.AirdropKeeper.GetActiveFunds(s.ctx)
-	if err != nil {
-		s.Require().FailNow("Failed to get active funds")
+	p2 := types.Params{
+		AllowList: []string{
+			verboseAddr.String(), // invalid address
+		},
 	}
-	genesis1 := types.NewGenesisState(p, activeFunds) // First genesis with no active funds
-	s.Require().NoError(genesis1.Validate())
-	amount := sdk.NewCoin("test", sdk.NewInt(1000))
-	fund := types.Fund{
-		Amount:     &amount,
-		DripAmount: sdk.NewInt(1),
+	funds := []types.ActiveFund{
+		{
+			Sender: addr.String(),
+			Fund: &types.Fund{
+				Amount: &sdk.Coin{
+					Denom:  "test",
+					Amount: sdk.NewInt(10),
+				},
+				DripAmount: sdk.NewInt(1),
+			},
+		},
 	}
-	s.Require().NoError(s.app.BankKeeper.SetBalance(s.ctx, addr, amount))
-	s.Require().NoError(s.app.AirdropKeeper.AddFund(s.ctx, addr, fund))
-	genesis2 := types.NewGenesisState(p, activeFunds) // Second genesis with valid active funds
-	s.Require().NoError(genesis2.Validate())
-
+	gen1 := types.NewGenesisState(p1, funds)
+	gen2 := types.NewGenesisState(p2, funds)
+	require.NoError(t, gen1.Validate())
+	require.Error(t, gen2.Validate())
 }
 
 func TestGenesisTestSuite(t *testing.T) {
