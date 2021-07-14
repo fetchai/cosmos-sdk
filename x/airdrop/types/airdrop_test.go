087d0e73a6357f3c489bfc8b61fc8fbe402f7e44
Joey <jonathan.sumner@fetch.ai>
2021-07-14 14:50:30 +0100

    Test: add test for Airdrop Drip

--- x/airdrop/types/airdrop_test.go
+++ x/airdrop/types/airdrop_test.go
@@ -1,28 +1,29 @@
 package types
 
 import (
+	"testing"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 type AirdropTestSuite struct {
 	suite.Suite
 }
 
-func TestFundValidateBasic (t *testing.T) {
+func TestFundValidateBasic(t *testing.T) {
 	amount := sdk.NewInt64Coin("test", 1)
-	fundPosDrip := Fund {
-		Amount: &amount,
+	fundPosDrip := Fund{
+		Amount:     &amount,
 		DripAmount: sdk.NewInt(1),
 	}
-	fundNilDrip := Fund {
-		Amount: &amount,
+	fundNilDrip := Fund{
+		Amount:     &amount,
 		DripAmount: sdk.NewInt(0),
 	}
-	fundNegDrip := Fund {
-		Amount: &amount,
+	fundNegDrip := Fund{
+		Amount:     &amount,
 		DripAmount: sdk.NewInt(-1),
 	}
 	require.NoError(t, fundPosDrip.ValidateBasic())
