35c9d65b752c4f346112c0c9dc2baaa1d916eeb5
Joey <jonathan.sumner@fetch.ai>
2021-07-14 14:23:15 +0100

    Test: add test for fund ValidateBasic

---
+++ x/airdrop/types/airdrop_test.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type AirdropTestSuite struct {
+	suite.Suite
+}
+
+func TestFundValidateBasic (t *testing.T) {
+	amount := sdk.NewInt64Coin("test", 1)
+	fundPosDrip := Fund {
+		Amount: &amount,
+		DripAmount: sdk.NewInt(1),
+	}
+	fundNilDrip := Fund {
+		Amount: &amount,
+		DripAmount: sdk.NewInt(0),
+	}
+	fundNegDrip := Fund {
+		Amount: &amount,
+		DripAmount: sdk.NewInt(-1),
+	}
+	require.NoError(t, fundPosDrip.ValidateBasic())
+	require.Error(t, fundNilDrip.ValidateBasic())
+	require.Error(t, fundNegDrip.ValidateBasic())
+}
+
+func TestAirdropTestSuite(t *testing.T) {
+	suite.Run(t, new(AirdropTestSuite))
+}
