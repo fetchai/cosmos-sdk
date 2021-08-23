450657cfe6a25c08e8e4df059865c368b802886e
kitty <jia.liu@fetch.ai>
2021-08-23 16:35:02 +0100

    change codespace for invalid pop error

--- types/errors/errors.go
+++ types/errors/errors.go
@@ -135,7 +135,7 @@ var (
 	ErrNotSupported = Register(RootCodespace, 37, "feature not supported")
 
 	// ErrInvalidPop to doc
-	ErrInvalidPop = Register(RootCodespace, 38, "invalid pop for public key")
+	ErrInvalidPop = Register(RootCodespace, 111000, "invalid pop for public key")
 
 	// ErrPanic is only set when we recover from a panic, so we know to
 	// redact potentially sensitive system info
