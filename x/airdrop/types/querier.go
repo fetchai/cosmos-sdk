463456028592896a0f87a1ecc617b34fc5471ff3
Flavien Binet <flavien.binet@gmail.com>
2022-02-28 18:20:20 +0100

    chores: drop airdrop module

--- x/airdrop/types/querier.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import (
-	"github.com/cosmos/cosmos-sdk/types/query"
-)
-
-func NewQueryAllFundsRequest(req *query.PageRequest) *QueryAllFundsRequest {
-	return &QueryAllFundsRequest{Pagination: req}
-}
