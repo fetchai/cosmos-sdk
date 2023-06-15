b65c2229ff0d93e601b9ea2c3df7e4a63fe4b50a
jonathansumner <jonathansumner98@gmail.com>
2023-06-15 21:03:04 +0100

    chore: simplify inflations_test.go
    Signed-off-by: jonathansumner <jonathansumner98@gmail.com>

--- x/mint/types/inflations_test.go
+++ x/mint/types/inflations_test.go
@@ -1,11 +1,9 @@
 package types
 
 import (
-	"fmt"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/mint"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -43,24 +41,29 @@ func (suite *SimTestSuite) testHandleInflations() {
 
 	targetAccounts := suite.getTestingAccounts(r, 3)
 	minter := suite.app.MintKeeper.GetMinter(suite.ctx)
+	params := suite.app.MintKeeper.GetParams(suite.ctx)
 	var testSupply int64 = 1000000
 
 	tests := []struct {
 		coins           sdk.Coins
 		inflation       Inflation
 		expectedBalance sdk.Int
-		expectedToPass  bool
 	}{
-		{sdk.NewCoins(sdk.NewCoin("denom-1", sdk.NewInt(testSupply))), NewInflation("denom-1", targetAccounts[0].Address.String(), sdk.NewDecWithPrec(1, 2)), sdk.NewInt(0), true},
-		{sdk.NewCoins(sdk.NewCoin("denom-2", sdk.NewInt(testSupply))), NewInflation("denom-2", targetAccounts[1].Address.String(), sdk.NewDecWithPrec(2, 2)), sdk.NewInt(0), true},
-		{sdk.NewCoins(sdk.NewCoin("denom-3", sdk.NewInt(testSupply))), NewInflation("denom-3", targetAccounts[2].Address.String(), sdk.NewDecWithPrec(3, 2)), sdk.NewInt(0), true},
+		{sdk.NewCoins(sdk.NewCoin("one", sdk.NewInt(testSupply))), NewInflation("one", targetAccounts[0].Address.String(), sdk.NewDecWithPrec(1, 2)), sdk.NewInt(0)},
+		{sdk.NewCoins(sdk.NewCoin("two", sdk.NewInt(testSupply))), NewInflation("two", targetAccounts[1].Address.String(), sdk.NewDecWithPrec(2, 2)), sdk.NewInt(0)},
+		{sdk.NewCoins(sdk.NewCoin("three", sdk.NewInt(testSupply))), NewInflation("three", targetAccounts[2].Address.String(), sdk.NewDecWithPrec(3, 2)), sdk.NewInt(0)},
 	}
 	for i, tc := range tests {
 		minter.Inflations = []*Inflation{&tc.inflation}
 		require.NoError(suite.T(), suite.app.BankKeeper.MintCoins(suite.ctx, ModuleName, tc.coins))
-		require.NotPanics(suite.T(), func() { mint.HandleInflations(suite.ctx, suite.app.MintKeeper) })
 
-		testAccountBalance := suite.app.BankKeeper.GetBalance(suite.ctx, targetAccounts[i].Address, fmt.Sprintf("denom-%d", i+1))
+		newCoinsToSend, err := CalculateInflation(&tc.inflation, params.BlocksPerYear, suite.app.BankKeeper.GetSupply(suite.ctx, tc.inflation.Denom))
+		require.NoError(suite.T(), err)
+
+		err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, ModuleName, sdk.AccAddress(tc.inflation.TargetAddress), newCoinsToSend)
+		require.NoError(suite.T(), err)
+
+		testAccountBalance := suite.app.BankKeeper.GetBalance(suite.ctx, targetAccounts[i].Address, tc.inflation.Denom)
 		require.Equal(suite.T(), tc.expectedBalance, testAccountBalance.Amount)
 	}
 }
@@ -91,3 +94,5 @@ func (suite *SimTestSuite) testInflationsValidation() {
 		}
 	}
 }
+
+// TODO(JS): different approaches of calculating inflations - timings + precision, test many different numbers
