fa16e3624e5f854f1db413d95baa088526e85691
void* <peter.bukva@gmail.com>
2023-07-17 11:15:02 +0100

    fix: Municipal Inflation: cli tests for query with denom (#156)

--- x/mint/client/testutil/suite.go
+++ x/mint/client/testutil/suite.go
@@ -103,17 +103,30 @@ func (s *IntegrationTestSuite) TestGetCmdQueryMunicipalInflation() {
 		expectedOutput string
 	}{
 		{
-			"json output",
+			"full - json output",
 			[]string{fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
 			`{"inflations":{"denom0":{"target_address":"cosmos1d9pzg5542spe4anjgu2zmk7wxhgh04ysn2phpq","inflation":"1.230000000000000000"},"denom1":{"target_address":"cosmos12kdu2sy0zcmz84qymyj6zcfvwss3a703xgpczm","inflation":"0.034500000000000000"}}}`,
 		},
 		{
-			"text output",
+			"full - text output",
 			[]string{fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=text", tmcli.OutputFlag)},
 			`inflations:
   denom0:
     inflation: "1.230000000000000000"
     target_address: cosmos1d9pzg5542spe4anjgu2zmk7wxhgh04ysn2phpq
+  denom1:
+    inflation: "0.034500000000000000"
+    target_address: cosmos12kdu2sy0zcmz84qymyj6zcfvwss3a703xgpczm`,
+		},
+		{
+			"selected denom - json output",
+			[]string{"denom1", fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
+			`{"inflations":{"denom1":{"target_address":"cosmos12kdu2sy0zcmz84qymyj6zcfvwss3a703xgpczm","inflation":"0.034500000000000000"}}}`,
+		},
+		{
+			"selected denom - text output",
+			[]string{"denom1", fmt.Sprintf("--%s=1", flags.FlagHeight), fmt.Sprintf("--%s=text", tmcli.OutputFlag)},
+			`inflations:
   denom1:
     inflation: "0.034500000000000000"
     target_address: cosmos12kdu2sy0zcmz84qymyj6zcfvwss3a703xgpczm`,
